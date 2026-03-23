113787be53011598077bf5bd3cc00ffcd10f7921
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2026-03-23 08:55:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,9 +66,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://Rbc1:Rbc1@rbc1.ksm7xyp.mongodb.net/Auto5?retryWrites=true&w=majority")   # This Db Is For Other Data Store
+O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://Rbc1:Rbc1@rbc1.ksm7xyp.mongodb.net/?retryWrites=true&w=majority")   # This Db Is For Other Data Store
 F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://Rbc3:Rbc3@rbc3.ivkjbye.mongodb.net/?appName=Rbc3")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://EllisaHub:EllisaHub@ellisahub.ncilv.mongodb.net/?appName=EllisaHub")   # This Db is for File Data Store When First Db Is Going To Be Full.
+S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://EllisaHub:EllisaHub@ellisahub.ncilv.mongodb.net/?retryWrites=true&w=majority")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
